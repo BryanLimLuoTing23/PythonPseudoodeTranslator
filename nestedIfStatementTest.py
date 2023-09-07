125daf8be16619fbcdb04cda49a9787e1b729c69
Bryan Lim <112593926+BryanLimLuoTing23@users.noreply.github.com>
2023-09-07 22:23:49 +0800

    Create nestedIfStatementTest.py

---
+++ nestedIfStatementTest.py
@@ -0,0 +1,15 @@
+foo = "foo"
+boy = "God"
+if foo == "foo":
+    print("Correct")
+    fook = input("Enter")
+    if foo == "dwef":
+        print("do something")
+        if bangbang == "wefr":
+            print("Please")
+
+foo = "Last line"
+
+
+# Need a way to detect how python breaks out of if loops
+#Need to detect when the indentation is reduced
